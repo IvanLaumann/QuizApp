820b80ef94a9dc027ed17fe30a1f8e4210fddb34
IvanLaumann <laumann.ivan@gmail.com>
2023-11-07 02:43:02 +0100

    answer() function added

--- script.js
+++ script.js
@@ -218,3 +218,17 @@ function showQuestion() {
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    console.log('Selected answer ist', selection);
+    let selectedQuestionNumber = selection.slice(-1);
+    console.log('selectedQuestionNumber is ', selectedQuestionNumber);
+    console.log('Current question is', question['right_answer']);
+
+    if (selectedQuestionNumber == question['right_answer']) {
+        console.log('Richtige Antwort!');
+    }else {
+        console.log('Leider falsch...');
+    }
+}
