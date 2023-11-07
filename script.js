aefa652f1e5489e5dd0c4151c371daaf1d4bbfa7
IvanLaumann <laumann.ivan@gmail.com>
2023-11-07 19:15:20 +0100

    nextQuestion & resetAnswerButtons added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const questions = [
     {
-        "question": "Wer hat HTML erfunden",
+        "question": "Wer hat HTML erfunden?",
         "answer_1": "Robbie Williams",
         "answer_2": "Lady Gaga",
         "answer_3": "Tim Berners-Lee",
@@ -8,7 +8,7 @@ const questions = [
         "right_answer": 3
     },
     {
-        "question": "Welche Programmiersprache wird hauptsächlich für das Styling von Webseiten verwendet",
+        "question": "Welche Programmiersprache wird hauptsächlich für das Styling von Webseiten verwendet?",
         "answer_1": "HTML",
         "answer_2": "CSS",
         "answer_3": "JavaScript",
@@ -16,7 +16,7 @@ const questions = [
         "right_answer": 2
     },
     {
-        "question": "Welche Programmiersprache ermöglicht die Interaktion auf Webseiten",
+        "question": "Welche Programmiersprache ermöglicht die Interaktion auf Webseiten?",
         "answer_1": "HTML",
         "answer_2": "CSS",
         "answer_3": "JavaScript",
@@ -24,7 +24,7 @@ const questions = [
         "right_answer": 3
     },
     {
-        "question": "Wofür steht CSS in der Webentwicklung",
+        "question": "Wofür steht CSS in der Webentwicklung?",
         "answer_1": "Cascading Style Sheets",
         "answer_2": "Creative Scripting System",
         "answer_3": "Computer Style Software",
@@ -32,7 +32,7 @@ const questions = [
         "right_answer": 1
     },
     {
-        "question": "Welche der folgenden ist keine gültige JavaScript-Variablennamen",
+        "question": "Welche der folgenden ist keine gültige JavaScript-Variablennamen?",
         "answer_1": "myVariable",
         "answer_2": "3rdVariable",
         "answer_3": "user_name",
@@ -40,7 +40,7 @@ const questions = [
         "right_answer": 2
     },
     {
-        "question": "Welche Funktion wird in JavaScript verwendet, um Elemente auf einer Webseite zu selektieren",
+        "question": "Welche Funktion wird in JavaScript verwendet, um Elemente auf einer Webseite zu selektieren?",
         "answer_1": "getElementById()",
         "answer_2": "selectElement()",
         "answer_3": "findElement()",
@@ -48,7 +48,7 @@ const questions = [
         "right_answer": 1
     },
     {
-        "question": "Wie fügt man in CSS einen roten Hintergrund zu einem HTML-Element hinzu",
+        "question": "Wie fügt man in CSS einen roten Hintergrund zu einem HTML-Element hinzu?",
         "answer_1": "background-color: red;",
         "answer_2": "color: red;",
         "answer_3": "border: red;",
@@ -56,7 +56,7 @@ const questions = [
         "right_answer": 1
     },
     {
-        "question": "Welche Programmiersprache wird verwendet, um Logik und Interaktivität auf Webseiten hinzuzufügen",
+        "question": "Welche Programmiersprache wird verwendet, um Logik und Interaktivität auf Webseiten hinzuzufügen?",
         "answer_1": "HTML",
         "answer_2": "CSS",
         "answer_3": "JavaScript",
@@ -64,7 +64,7 @@ const questions = [
         "right_answer": 3
     },
     {
-        "question": "Was ist die Hauptfunktion von JavaScript",
+        "question": "Was ist die Hauptfunktion von JavaScript?",
         "answer_1": "Audioverarbeitung",
         "answer_2": "Serverkommunikation",
         "answer_3": "Bildbearbeitung",
@@ -72,7 +72,7 @@ const questions = [
         "right_answer": 4
     },
     {
-        "question": "Was ist der Zweck von JavaScript-Frameworks wie React oder Angular",
+        "question": "Was ist der Zweck von JavaScript-Frameworks wie React oder Angular?",
         "answer_1": "Serververwaltung",
         "answer_2": "Datenbankdesign",
         "answer_3": "Webseiten-Styling",
@@ -80,7 +80,7 @@ const questions = [
         "right_answer": 4
     },
     {
-        "question": "Welche CSS-Eigenschaft wird verwendet, um den Abstand zwischen den Elementen zu kontrollieren",
+        "question": "Welche CSS-Eigenschaft wird verwendet, um den Abstand zwischen den Elementen zu kontrollieren?",
         "answer_1": "padding",
         "answer_2": "margin",
         "answer_3": "border",
@@ -88,7 +88,7 @@ const questions = [
         "right_answer": 2
     },
     {
-        "question": "Welche Methode wird in JavaScript verwendet, um Text in Großbuchstaben umzuwandeln",
+        "question": "Welche Methode wird in JavaScript verwendet, um Text in Großbuchstaben umzuwandeln?",
         "answer_1": "toUpperCase()",
         "answer_2": "toLowerCase()",
         "answer_3": "capitalize()",
@@ -96,7 +96,7 @@ const questions = [
         "right_answer": 1
     },
     {
-        "question": "Was ist der Zweck des HTML-Elements '?'",
+        "question": "Was ist der Zweck des HTML-Elements '?'?",
         "answer_1": "Um eine Überschrift zu erstellen",
         "answer_2": "Um eine Verknüpfung zu einer anderen Webseite oder Datei zu erstellen",
         "answer_3": "Um ein Bild anzuzeigen",
@@ -104,7 +104,7 @@ const questions = [
         "right_answer": 2
     },
     {
-        "question": "Was ist die Funktion von CSS-Pseudoklassen wie ':hover'",
+        "question": "Was ist die Funktion von CSS-Pseudoklassen wie ':hover'?",
         "answer_1": "Text in fett anzeigen",
         "answer_2": "Elemente beim Klicken verstecken",
         "answer_3": "Styling bei Mausinteraktion ändern",
@@ -112,7 +112,7 @@ const questions = [
         "right_answer": 3
     },
     {
-        "question": "Welche JavaScript-Methode wird verwendet, um ein Element aus dem DOM zu entfernen",
+        "question": "Welche JavaScript-Methode wird verwendet, um ein Element aus dem DOM zu entfernen?",
         "answer_1": "deleteElement()",
         "answer_2": "removeChild()",
         "answer_3": "hideElement()",
@@ -120,7 +120,7 @@ const questions = [
         "right_answer": 2
     },
     {
-        "question": "Was ist das Ergebnis von '10' + 20 in JavaScript",
+        "question": "Was ist das Ergebnis von '10' + 20 in JavaScript?",
         "answer_1": "1020",
         "answer_2": "30",
         "answer_3": "102",
@@ -128,7 +128,7 @@ const questions = [
         "right_answer": 1
     },
     {
-        "question": "Welche Programmiersprache wird oft für serverseitige Webentwicklung verwendet",
+        "question": "Welche Programmiersprache wird oft für serverseitige Webentwicklung verwendet?",
         "answer_1": "HTML",
         "answer_2": "CSS",
         "answer_3": "JavaScript",
@@ -136,7 +136,7 @@ const questions = [
         "right_answer": 4
     },
     {
-        "question": "Was ist der Zweck von Media Queries in CSS",
+        "question": "Was ist der Zweck von Media Queries in CSS?",
         "answer_1": "Bildbearbeitung",
         "answer_2": "Serverkommunikation",
         "answer_3": "Anpassung des Layouts basierend auf dem Bildschirmgerät",
@@ -144,7 +144,7 @@ const questions = [
         "right_answer": 3
     },
     {
-        "question": "Was bedeutet 'DOM' in Bezug auf Webentwicklung",
+        "question": "Was bedeutet 'DOM' in Bezug auf Webentwicklung?",
         "answer_1": "Digital Object Model",
         "answer_2": "Document Object Model",
         "answer_3": "Dynamic Order Mechanism",
@@ -152,7 +152,7 @@ const questions = [
         "right_answer": 2
     },
     {
-        "question": "Welche Art von Daten können in JavaScript-Arrays gespeichert werden",
+        "question": "Welche Art von Daten können in JavaScript-Arrays gespeichert werden?",
         "answer_1": "Nur Zahlen",
         "answer_2": "Nur Text",
         "answer_3": "Verschiedene Datentypen",
@@ -160,7 +160,7 @@ const questions = [
         "right_answer": 3
     },
     {
-        "question": "Was ist der Zweck von 'localStorage' in JavaScript",
+        "question": "Was ist der Zweck von 'localStorage' in JavaScript?",
         "answer_1": "Zur Kommunikation mit einem Server",
         "answer_2": "Zum temporären Speichern von Daten im Browser",
         "answer_3": "Zur Verschlüsselung von Daten",
@@ -168,7 +168,7 @@ const questions = [
         "right_answer": 2
     },
     {
-        "question": "Welche Methode wird in JavaScript verwendet, um einen Wert in eine ganze Zahl (Integer) umzuwandeln",
+        "question": "Welche Methode wird in JavaScript verwendet, um einen Wert in eine ganze Zahl (Integer) umzuwandeln?",
         "answer_1": "parseInt()",
         "answer_2": "convertToInteger()",
         "answer_3": "toFloat()",
@@ -176,7 +176,7 @@ const questions = [
         "right_answer": 1
     },
     {
-        "question": "Was ist der Unterschied zwischen '==' und '===' in JavaScript",
+        "question": "Was ist der Unterschied zwischen '==' und '===' in JavaScript?",
         "answer_1": "Es gibt keinen Unterschied, sie sind austauschbar.",
         "answer_2": "'==' vergleicht den Wert, '===' vergleicht den Wert und den Datentyp.",
         "answer_3": "'==' vergleicht den Datentyp, '===' vergleicht den Wert.",
@@ -184,7 +184,7 @@ const questions = [
         "right_answer": 2
     },
     {
-        "question": "Was ist ein 'Callback' in JavaScript",
+        "question": "Was ist ein 'Callback' in JavaScript?",
         "answer_1": "Ein besonderes HTML-Element",
         "answer_2": "Eine Art von Kommentar im Code",
         "answer_3": "Eine Funktion, die als Argument an eine andere Funktion übergeben wird",
@@ -192,7 +192,7 @@ const questions = [
         "right_answer": 3
     },
     {
-        "question": "Was ist das Ziel von Responsive Webdesign",
+        "question": "Was ist das Ziel von Responsive Webdesign?",
         "answer_1": "Eine Website so schwer wie möglich zu machen",
         "answer_2": "Eine Website für Mobilgeräte anzupassen",
         "answer_3": "Eine Website für den Druck vorzubereiten",
@@ -234,4 +234,22 @@ function answer(selection) {
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
     document.getElementById('next-button').disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons();
+    showQuestion();
+}
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
