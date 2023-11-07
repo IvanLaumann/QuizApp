42ac5847fe81f5301c9760d4a6fa5ec9cc0f8a46
IvanLaumann <laumann.ivan@gmail.com>
2023-11-07 03:41:39 +0100

    next-button disabled & enabled

--- script.js
+++ script.js
@@ -237,4 +237,5 @@ function answer(selection) {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
+    document.getElementById('next-button').disabled = false;
 }
